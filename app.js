4a37f617632c38b487ef446e91f9bced23770293
squirrelinsouthern <squirrel_000@163.com>
2019-07-18 14:51:12 +0800

    老是给我报错！！

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
   globalData: {
-    userInfo: null
+    host: 'https://gp-server.hunger-valley.com',  
+    app_id: "wx35e23df4dfa81a58",
+    app_secret: "2e866a50d1c24666754963197d3402c1",
+    t_app_id: "SkWgamSkRoLoFJi7w9ruVt4M",
+    t_app_secret: "eqrSw7b8mcQSwoAuLTGFcx4f"
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,36 @@
-// pages/login/login.js
+const { http } = require('../../lib/http.js')
+const { app_id, app_secret } = getApp().globalData
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow() {
+    // http.get('/todos')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  
+  login(event){
+    let encrypted_data = event.detail.encryptedData
+    let iv = event.detail.iv
+    let code
+    wx.login({
+      success(res) {
+        code = res.code
+        console.log(code)
+        console.log(app_id)
+        http.post('/sign_in/mini_program_user', {
+          code,
+          iv,
+          encrypted_data,
+          app_id,
+          app_secret,
+        })
+        .then(response => {
+            wx.setStorageSync('me',response.data.resource)
+        })
+      }
+    })
+  
   }
+  
 })
