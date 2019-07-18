af13b5c1b33a9b452d9be4dec5cf1e6bd7ac22cb
squirrelinsouthern <39366943+squirrelinsouthern@users.noreply.github.com>
2019-07-18 21:24:22 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',  
-    app_id: "wx35e23df4dfa81a58",
-    app_secret: "2e866a50d1c24666754963197d3402c1",
-    t_app_id: "SkWgamSkRoLoFJi7w9ruVt4M",
-    t_app_secret: "eqrSw7b8mcQSwoAuLTGFcx4f"
-  }
-})
