36c7d499d16031a15e393dadee743f520c979213
squirrelinsouthern <squirrel_000@163.com>
2019-07-18 20:17:45 +0800

    搞清楚data&pramas,解决bug

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,14 @@
+// const app = getApp()
 const { http } = require('../../lib/http.js')
 const { app_id, app_secret } = getApp().globalData
 
 Page({
   data: {
   },
-
   onShow() {
-    // http.get('/todos')
+    http.get('/todos')
+    // http.post('/sign_in/mini_program_user', {
+    // })
   },
   
   login(event){
@@ -14,10 +16,8 @@ Page({
     let iv = event.detail.iv
     let code
     wx.login({
-      success(res) {
-        code = res.code
-        console.log(code)
-        console.log(app_id)
+      success:(response) => {
+        code = response.code
         http.post('/sign_in/mini_program_user', {
           code,
           iv,
@@ -25,12 +25,35 @@ Page({
           app_id,
           app_secret,
         })
-        .then(response => {
-            wx.setStorageSync('me',response.data.resource)
-        })
+        // console.log(response.response.data)
+        // .then(response => {
+          
+        //     wx.setStorageSync('me',response.data.resource)
+        // })
       }
     })
-  
   }
+  // login(e){
+  //   wx.login({
+  //     success:response=>this.onGetCode({ ...response, ...e.detail }),
+  //     fail: error => this.onError(error)
+  //   })
+  // },
+  // onGetCode({ code, encryptedData, iv}){
+  //   http.post('/sign_in/mini_program_user', {
+  //         code,
+  //         iv,
+  //         encrypted_data: encryptedData,
+  //         app_id,
+  //         app_secret,
+  //       })
+  //       .then(response => {
+  //           me = response.data.resource
+  //       })
+  // }
+
+
+
+
   
 })
